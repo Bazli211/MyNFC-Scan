3aa1f36e577e0f78f21e84dd822d8494db9b5703
Bazli211 <aimanbazli511@gmail.com>
2025-02-04 03:28:06 +0800

    Update form.blade.php

--- resources/views/fines/form.blade.php
+++ resources/views/fines/form.blade.php
@@ -7,33 +7,47 @@
                  @if ($fine->exists)
                  @method('PUT')
                   @endif
-                <!-- Student Matric Number -->
-                <div class="form-group">
+                  <div class="form-group">
                     <label for="student_matricNum">Student Matric Number</label>
-                    <input type="text" class="form-control" id="student_matricNum" name="student_matricNum" 
-                           value="{{ old('student_matricNum', $fine->student_matricNum ?? '') }}" required readonly>
-                </div>
+                    <input type="text" class="form-control" id="student_matricNum" name="student_matricNum"
+                        value="{{ old('student_matricNum', $fine->student_matricNum) }}" required readonly>
+                    </div>
 
-                <!-- Sticker ID -->
+               <!-- Sticker ID -->
                 <div class="form-group">
                     <label for="sticker_id">Sticker ID</label>
-                    <input type="text" class="form-control" id="sticker_id" name="sticker_id" 
-                           value="{{ old('sticker_id', $fine->sticker_id ?? '') }}" required readonly>
+                    <input type="text" class="form-control" id="sticker_id" name="sticker_id"
+                        value="{{ old('sticker_id', $fine->sticker_id) }}" required readonly>
                 </div>
 
-                <!-- Vehicle Type -->
+                 <!-- Session -->
+                 <div class="form-group">
+                    <label for="Session">Session Year</label>
+                    <input type="text" class="form-control" id="session" name="session"
+                        value="{{  old('session', $fine->session) }}">
+                </div>
+
+                <!-- Vehicle Plate Number -->
+                <div class="form-group">
+                    <label for="vehiclePlateNum">Vehicle Plate Number</label>
+                    <input type="text" class="form-control" id="vehiclePlateNum" name="vehiclePlateNum"
+                        value="{{ old('vehiclePlate', isset($fine->vehicle) ? $fine->vehicle->vehiclePlateNum : '') }}" readonly>
+                </div>
+
+               <!-- Vehicle Type -->
                 <div class="form-group">
                     <label for="vehicle_type">Vehicle Type</label>
-                    <input type="text" class="form-control" id="vehicle_type" name="vehicle_type" 
-                            value="{{ old('vehicle_type', $fine->vehicle_type ?? '') }}" readonly>
+                    <input type="text" class="form-control" id="vehicle_type" name="vehicle_type"
+                        value="{{ old('vehicle_type', isset($fine->vehicle) ? $fine->vehicle->vehicle_type : '') }}" readonly>
                 </div>
 
                 <!-- Vehicle Brand -->
                 <div class="form-group">
                     <label for="vehicle_brand">Vehicle Brand</label>
-                    <input type="text" class="form-control" id="vehicle_brand" name="vehicle_brand" 
-                        value="{{ old('vehicle_brand', $fine->vehicle_brand ?? '') }}" readonly>
+                    <input type="text" class="form-control" id="vehicle_brand" name="vehicle_brand"
+                            value="{{ old('vehicle_brand', isset($fine->vehicle) ? $fine->vehicle->vehicle_brand : '') }}" readonly>
                 </div>
+
 
                 <!-- Fine Date -->
                 <div class="form-group">
@@ -46,7 +60,7 @@
                 <div class="form-group">
                     <label for="fine_time">Fine Time</label>
                     <input type="time" class="form-control" id="fine_time" name="fine_time" 
-                           value="{{ old('fine_time', $fine->fine_time ?? now()->format('H:i')) }}" required>
+                           value="{{ old('fine_time', $fine->fine_time ?? now()->format('H:i:s')) }}" required>
                 </div>
 
                 <!-- Location -->
@@ -72,7 +86,7 @@
 
                 <!-- Kesalahan (Offenses) -->
                 <div class="form-group">
-                    <label for="kesalahan">Kesalahan (Offenses)</label>
+                    <label for="kesalahan">Offenses</label>
                     <div id="kesalahan">
                         @php
                             $kesalahanOptions = [
@@ -81,7 +95,7 @@
                                 "3. MENGHALANG LALUAN",
                                 "4. TIADA LESEN MEMANDU/TAMAT TEMPOH",
                                 "5. LESEN L MEMBAWA PEMBONCENG",
-                                "6. TIADA CUKAI JALAN YANG SAH/TAMAT TEMPOH",
+                                "6. TIADA CUKAI JALAN YANG SAH/TAMAT TEMPOH ATAU TARIKH STICKER SUDAH TAMAT",
                                 "7. MELANGGAR JALAN SEHALA/DILARANG MASUK",
                                 "8. TIDAK MEMAKAI TOPI KELEDAR (PENUNGGANG/PEMBONCENG)",
                                 "9. TIADA PELEKAT UITM SESI",
@@ -102,33 +116,34 @@
                     </div>
                 </div>
 
-                <!-- Nama Pelajar -->
-<div class="form-group">
-    <label for="nama_pelajar">Nama Pelajar</label>
-    <input type="text" class="form-control" id="nama_pelajar" name="nama_pelajar" 
-           value="{{ old('nama_pelajar', $fine->nama_pelajar ?? '') }}" readonly>
-</div>
+            <!-- Nama Pelajar -->
+            <div class="form-group">
+                <label for="nama_pelajar">Student Name</label>
+                <input type="text" class="form-control" id="nama_pelajar" name="nama_pelajar"
+                    value="{{ old('nama_pelajar', isset($fine->student) ? $fine->student->name : '') }}" readonly>
+            </div>
 
-<!-- Kod Program -->
-<div class="form-group">
-    <label for="kod_program">Kod Program</label>
-    <input type="text" class="form-control" id="kod_program" name="kod_program" 
-           value="{{ old('kod_program', $fine->kod_program ?? '') }}" readonly>
-</div>
 
-<!-- Fakulti -->
-<div class="form-group">
-    <label for="fakulti">Fakulti</label>
-    <input type="text" class="form-control" id="fakulti" name="fakulti" 
-           value="{{ old('fakulti', $fine->fakulti ?? '') }}" readonly>
-</div>
+            <!-- Kod Program -->
+            <div class="form-group">
+                <label for="kod_program">Code Programme</label>
+                <input type="text" class="form-control" id="kod_program" name="kod_program"
+                    value="{{ old('kod_program', isset($fine->student) ? $fine->student->kod_program : '') }}" readonly>
+            </div>
+            <!-- Fakulti -->
+            <div class="form-group">
+                <label for="fakulti">Faculty</label>
+                <input type="text" class="form-control" id="fakulti" name="fakulti"
+                    value="{{ old('fakulti', isset($fine->student)?$fine->student->fakulti : '') }}" readonly>
+            </div>
 
-<!-- Kolej -->
-<div class="form-group">
-    <label for="kolej">Kolej</label>
-    <input type="text" class="form-control" id="kolej" name="kolej" 
-           value="{{ old('kolej', $fine->kolej ?? '') }}" readonly>
-</div>
+            <!-- Kolej -->
+            <div class="form-group">
+                <label for="kolej">College</label>
+                <input type="text" class="form-control" id="kolej" name="kolej"
+                    value="{{ old('kolej', isset($fine->student)?$fine->student->kolej : '') }}" readonly>
+            </div>
+
 <!-- Di Kunci/Di Saman -->
 <div class="form-group">
     <label for="di_kunci_di_saman">Di Kunci / Di Saman</label>
@@ -140,7 +155,7 @@
 </div>
 <!-- Dikompaun -->
 <div class="form-group">
-    <label for="dikompaun">Dikompaun</label>
+    <label for="dikompaun">Compounded</label>
     <select class="form-control" id="dikompaun" name="dikompaun" required>
         <option value="" disabled selected>Select Option</option>
         <option value="Yes" {{ old('dikompaun', $fine->dikompaun ?? '') == 'Yes' ? 'selected' : '' }}>Yes</option>
