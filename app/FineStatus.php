e0ec1fc599e864c88d91ff7f4e70217572819365
Bazli211 <aimanbazli511@gmail.com>
2025-02-04 03:57:02 +0800

    Update FineStatus.php

--- app/FineStatus.php
+++ app/FineStatus.php
@@ -2,19 +2,25 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FineStatus extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
-        'student_matricNumber', 'fine_details', 'fine_date', 'fine_time', 'fine_status'
+        'student_matricNumber', 'fine_details', 'fine_date', 'fine_time', 'fine_status','kesalahan','nama_pelajar','kod_program','kolej','fakulti'
+    ];
+
+    protected $casts = [
+        'fine_details' => 'array', // Cast fine_details to array
+        'kesalahan' => 'array', // Cast kesalahan to array
     ];
 
     public function fine()
     {
         return $this->belongsTo(Fine::class, 'student_matricNumber', 'student_matricNum');
     }
+    public function user()
+{
+    return $this->belongsTo(User::class, 'student_matricNumber', 'matric_number');
+}
 }
