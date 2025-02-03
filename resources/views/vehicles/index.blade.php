19f6c509e8333372d6fade88ea0ef64acfb1a79e
Bazli211 <aimanbazli511@gmail.com>
2025-02-04 03:52:35 +0800

    Update index.blade.php

--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -5,34 +5,99 @@
 <div class="container">
     <h2>Your Vehicle Information</h2>
 
-    @if ($vehicle)
-        <table class="table table-bordered">
-            <tr>
-                <th>Vehicle Plate Number</th>
-                <td>{{ $vehicle->vehiclePlateNum }}</td>
-            </tr>
-            <tr>
-                <th>Vehicle Brand</th>
-                <td>{{ $vehicle->vehicle_brand }}</td>
-            </tr>
-            <tr>
-                <th>Sticker Date</th>
-                <td>{{ $vehicle->sticker_date }}</td>
-            </tr>
-            <tr>
-                <th>Vehicle Color</th>
-                <td>{{ $vehicle->vehicle_color }}</td>
-            </tr>
-            <tr>
-                <th>Road Tax Date</th>
-                <td>{{ $vehicle->roadtax_date }}</td>
-            </tr>
-        </table>
+{{-- Warning for expired road tax --}}
+@if($warnings->isNotEmpty())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach($warnings as $warning)
+                <li>{{ $warning }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 
-        <a href="{{ route('vehicles.edit', $vehicle->id) }}" class="btn btn-warning">Edit Vehicle</a>
-    @else
-        <p>You have not added any vehicle information yet.</p>
+@if ($vehicle)
+    {{-- Display vehicle details --}}
+    <table class="table table-bordered">
+    <tr>
+        <th>Vehicle Plate Number</th>
+        <td>{{ $vehicle->vehiclePlateNum }}</td>
+    </tr>
+    <tr>
+        <th>Vehicle Brand</th>
+        <td>{{ $vehicle->vehicle_brand }}</td>
+    </tr>
+    <tr>
+        <th>Vehicle Type</th>
+        <td>{{ $vehicle->vehicle_type }}</td>
+    </tr>
+    <tr>
+        <th>Vehicle Model</th>
+        <td>
+            @if($vehicle->vehicle_type === 'motorcycle')
+                {{ $vehicle->motorcycle_model }}
+            @elseif($vehicle->vehicle_type === 'car')
+                {{ $vehicle->car_model }}
+            @else
+                N/A
+            @endif
+        </td>
+    </tr>
+    <tr>
+        <th>Sticker Date</th>
+        <td>{{ $vehicle->sticker_date }}</td>
+    </tr>
+    <tr>
+        <th>Vehicle Color</th>
+        <td>{{ $vehicle->vehicle_color }}</td>
+    </tr>
+    <tr>
+        <th>Road Tax Date</th>
+        <td>{{ $vehicle->roadtax_date }}</td>
+    </tr>
+</table>
+
+    {{-- Edit button --}}
+    <a href="{{ route('vehicles.edit', $vehicle->id) }}" class="btn btn-warning">Edit Vehicle</a>
+@else
+    {{-- Add vehicle logic --}}
+    @if(!$warnings->contains('No sticker found.') && !$warnings->contains('Your sticker request is pending approval.'))
+        {{-- Allow the user to add a vehicle if no sticker warnings exist --}}
         <a href="{{ route('vehicles.create') }}" class="btn btn-primary">Add Vehicle</a>
+    @else
+        {{-- Disable the button and show a warning when clicked --}}
+        <button class="btn btn-primary" onclick="showStickerWarning()" type="button">Add Vehicle</button>
     @endif
+@endif
+
+@section('scripts')
+<script>
+    function showStickerWarning() {
+        let message = "You cannot add a vehicle because:\n";
+        @if ($warnings->contains('No sticker found.'))
+            message += "- No sticker found. Please apply for a sticker.\n";
+        @endif
+        @if ($warnings->contains('Your sticker request is pending approval.'))
+            message += "- Your sticker request is pending approval.\n";
+        @endif
+        alert(message);
+    }
+</script>
+@endsection
 </div>
+@section('scripts')
+<script>
+    function showStickerWarning() {
+        let message = "You cannot add a vehicle because:\n";
+        @if ($warnings->contains('No sticker found.'))
+            message += "- No sticker found. Please apply for a sticker.\n";
+        @endif
+        @if ($warnings->contains('Your sticker request is pending approval.'))
+            message += "- Your sticker request is pending approval.\n";
+        @endif
+        alert(message);
+    }
+</script>
+@endsection
+
 @endsection
