d9c38c917478603c939c43784d287d70f37500d6
Bazli211 <aimanbazli511@gmail.com>
2025-02-04 03:14:16 +0800

    Update dashboard.blade.php

--- resources/views/police/dashboard.blade.php
+++ resources/views/police/dashboard.blade.php
@@ -12,20 +12,19 @@
             <p>Use the menu below to navigate:</p>
 
             <ul class="list-unstyled">
-                <li><a href="{{ route('fines.index') }}" class="btn btn-primary btn-block mb-2">Manage Fine</a></li>
                 <li><a href="{{ route('police.stickers.index') }}" class="btn btn-info btn-block mb-2">Manage Stickers</a></li>
-                <li>
-                    <a href="{{ route('nfc.scan') }}" class="btn btn-success btn-block mb-2">
-                        NFC Scan for Fine
-                    </a>
-                </li>
+                <li><a href="{{ route('nfc.scan') }}" class="btn btn-success btn-block mb-2">NFC Scan for Fine</a></li>
+                <li><a href="{{ route('fines.index') }}" class="btn btn-primary btn-block mb-2">Manage Fine</a></li>
+                <!--
                 <li>
                     <a href="{{ route('nfc.write') }}" class="btn btn-warning btn-block">
                         Write NFC Sticker
                     </a>
                 </li>
+               -->
             </ul>
         </div>
     </div>
 </div>
 @endsection
+
