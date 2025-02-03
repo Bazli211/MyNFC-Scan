c894dddf512a8d4db945419b8b240a417c6b3129
Bazli211 <aimanbazli511@gmail.com>
2025-02-04 03:13:35 +0800

    Update index.blade.php

--- resources/views/stickers/index.blade.php
+++ resources/views/stickers/index.blade.php
@@ -3,16 +3,34 @@
 @section('content')
 <div class="container">
     <h1>Your Sticker Requests</h1>
-    @foreach ($stickers as $sticker)
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5>{{ $sticker->unique_id }}</h5> <!-- Use unique_id here -->
-                <p>Status: {{ $sticker->status_sticker }}</p>
-                <p>Validity: {{ $sticker->validity_date }}</p>
-                <a href="{{ route('police.stickers.show', $sticker->unique_id) }}" class="btn btn-info">View Details</a>
-            </div>
+
+    @if ($stickers->isEmpty())
+        <div class="alert alert-warning">
+            <p>You haven't created any stickers yet. Click the button below to request a new sticker.</p>
+            <a href="{{ route('stickers.create') }}" class="btn btn-primary">Create Sticker</a>
         </div>
-    @endforeach
+    @else
+        @foreach ($stickers as $sticker)
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h5>Sticker ID: {{ $sticker->unique_id }}</h5>
+                    <p>Status: {{ $sticker->status_sticker }}</p>
+                    <p>Requested Date: {{ $sticker->requested_date }}</p>
+                    <p>Accepted Date: {{ $sticker->accepted_date }}</p>
+                    <p>Expired Date: {{ $sticker->expired_date }}</p>
+                </div>
+            </div>
+        @endforeach
+        <div class="mt-3 d-flex">
+    {{-- Renew Sticker Button --}}
+                <a href="{{ route('stickers.renew', $stickers->last()->unique_id) }}" class="btn btn-primary me-3">Renew Sticker</a>
+    
+    {{-- Create Another Sticker Button (only if the last sticker is rejected) --}}
+            @if ($stickers->last()->status_sticker === 'rejected')
+                <a href="{{ route('stickers.create') }}" class="btn btn-primary">Create Another Sticker</a>
+        @endif
+    </div>
+    @endif
 </div>
 @endsection
 
